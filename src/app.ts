d2a77872b1cc7a6302cd7338ac65bfa544967150
Julia Sales <juliasalesc@outlook.com>
2026-06-14 04:19:56 -0300

    adicionado funcionalidade de listar produto

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import Fastify from 'fastify'
 import 'dotenv/config'
 import cors from '@fastify/cors';
 import helmet from '@fastify/helmet';
+import productRoutes from './routes/products.routes';
 
 const PORT = parseInt(process.env.PORT ?? '3000');
 
@@ -20,6 +21,8 @@ fastify.register(cors,{
 fastify.register(helmet, {
     contentSecurityPolicy: false
 });
+
+fastify.register(productRoutes, { prefix: '/products' });
 
 // Declare a route
 fastify.get('/', async (request, reply) => {
